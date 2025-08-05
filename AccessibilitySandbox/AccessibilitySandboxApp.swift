cac827e1a63b12b8e3f62a20f1cb08d8964313cf
philip713 <105677806+philip713@users.noreply.github.com>
2025-08-04 22:45:18 -0600

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Philip Janzel Paradeza on 2025-08-04.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Philip Janzel Paradeza on 2025-08-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
